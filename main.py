480c7ca273685db9334155b56bbd9d8a19f864db
chengyouhan <chengyouhan20090612@gmail.com>
2026-04-29 16:43:11 +0800

    feat(main): 實作 ChatOpenAI 初始化與初步調用測試

--- main.py
+++ main.py
@@ -8,11 +8,23 @@ def main():
     print(f"My name is {name}")
     load_dotenv()
     api_key = os.getenv("OPENAI_API_KEY")
+    
     if api_key:
         print("OPENAI_API_KEY is set")
     else:
         print("OPENAI_API_KEY is not set")
         return
+
+    llm = ChatOpenAI(
+        model = os.getenv("MODEL"),
+        temperature = float(os.getenv("TEMPERATURE", 0.5)),
+        base_url=os.getenv("BASE_URL"),
+        api_key=os.getenv("OPENAI_API_KEY")
+    )
+    
+    response = llm.invoke("用一句繁體中文自我介紹。")
+    print(response.content)
+
 
 if __name__ == "__main__":
     main()
