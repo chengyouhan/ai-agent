019af7f774cfd254a4e2a510aefd0ec44dc5c028
chengyouhan <chengyouhan20090612@gmail.com>
2026-04-29 14:18:16 +0800

    refactor(project): 調整環境變數載入與 API key 檢查邏輯

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+import os
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+load_dotenv()
+os.getenv("OPENAI_API_KEY","Hello")
+
 def main():
     name = "AI Agent"
     print("Hello from ai-agent!")
     print(f"My name is {name}")
+    print(os.getenv("OPENAI_API_KEY"))
+    print(os.getenv("Hello","OPENAI_API_KEY"))
 
 if __name__ == "__main__":
     main()
