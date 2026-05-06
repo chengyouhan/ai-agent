c000cfca4d16b4563a1ff32864a03181a029f004
chengyouhan <chengyouhan20090612@gmail.com>
2026-05-06 15:33:38 +0800

    feat(main): 實作對話紀錄功能以支援多輪對話

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
+from langchain_core.messages import AIMessage, HumanMessage
 
 def main():
     name = "AI Agent"
@@ -22,18 +23,28 @@ def main():
         api_key=os.getenv("OPENAI_API_KEY")
     )
     
+    messages = []
     while True:
         user_input = input("You: ").strip()
         if user_input == "q" or user_input == "bye":
             print("bye")
             break
+
+        human_message= HumanMessage(content=user_input)
+        context_message = [*messages,human_message]
+        
         print("Thinking...")
         print("AI: ", end="", flush=True)
-        for chunk in llm.stream(user_input):
+        reply_parts: list[str] = []
+        for chunk in llm.stream(context_message):
             print(chunk.content, end="", flush=True)
-        print("\n")
-
-
+            reply_parts.append(chunk.content)
+        print("")
+        assistant_text = "".join(reply_parts)
+        assistant_message = AIMessage(content=assistant_text)
+        messages.append(human_message)
+        messages.append(assistant_message)
+    
 if __name__ == "__main__":
     main()
     
